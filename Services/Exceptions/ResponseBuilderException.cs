6e03d7f77fb513c8494a5f88a9b2b7900a6b6ec6
HTFDT <almetov_tag@mail.ru>
2025-01-04 22:04:57 +0500

    response builder

---
+++ Services/Exceptions/ResponseBuilderException.cs
@@ -0,0 +1,3 @@
+﻿namespace Services.Exceptions;
+
+internal class ResponseBuilderException(string message) : Exception(message);
---
+++ Services/Helpers/ApiResponseBuilder/IApiResponseBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Services.Helpers.ResponseBuilder;
+
+public interface IApiResponseBuilder<out TBuilder, out T> : IBuilder<T> where TBuilder: IBuilder<T>
+{
+    TBuilder NoContent();
+    TBuilder Success();
+    TBuilder Error(int statusCode, string? message = null);
+}
+
+public interface IApiResponseBuilder<out T> : IBuilder<T>
+{
+    void NoContent();
+    void Success();
+    void Error(int statusCode, string? message = null);
+}
