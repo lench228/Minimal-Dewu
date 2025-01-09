19ffd35f73be678d574fdb9c5e20b669c115c0ed
HTFDT <almetov_tag@mail.ru>
2025-01-09 19:29:15 +0500

    migration

--- Infrastructure/EfCore/Migrations/20250109141731_AddUserAccountData.Designer.cs
+++ Infrastructure/EfCore/Migrations/20250109141731_AddUserAccountData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.EfCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.EfCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250109141731_AddUserAccountData")]
+    partial class AddUserAccountData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
